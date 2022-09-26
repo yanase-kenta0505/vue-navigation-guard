cf6ae5d82e4068e726d2d99c98f2ad288aefb350
yanasekenta <ynsknt0505@gmail.com>
2022-09-26 22:34:33 +0900

    routerを追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
 </template>
 
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from './components/home.vue'
+import About from './components/About.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
